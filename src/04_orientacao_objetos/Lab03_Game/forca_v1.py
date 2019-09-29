771ac9505e11de09d5a340f12050e7b1395dd3d0
Rafael Lima <ls.rafael@icloud.com>
2019-09-29 20:03:42 -0300

    Detalhamento dos primeiros passos

--- src/04_orientacao_objetos/Lab03_Game/forca_v1.py
+++ src/04_orientacao_objetos/Lab03_Game/forca_v1.py
@@ -66,3 +66,36 @@ board = [ '''
 / \  |
      |
 =========''' ]
+
+# Class
+class Hangman:
+
+    # Contructor
+    def __init__(self, word):
+
+    # Method to guess the letter
+    def guess(self, letter):
+
+    # Method to check if game is over
+    def hangman_over(self):
+
+    # Method to check if the player won
+    def hangman_won(self):
+
+    # Method to hide the letter on the board
+    def hide_word(self):
+
+    # Method to check the status game and print the board on the screen
+    def print_game_status(self):
+
+# Function to read a word randomly from the word bank
+def rand_word():
+    with open('palavras.txt','rt') as f:
+        bank = f.readlines()
+    return bank[random.randint(0, len(bank))].strip()
+
+# Main function
+def main():
+    # Object
+    game = Hangman(rand_word())
+
