052f04a9efcc79b1cade10524ee571ea8a7166fd
Rafael Lima <ls.rafael@icloud.com>
2019-09-30 23:42:10 -0300

    Ajuda com o código

--- src/04_orientacao_objetos/Lab03_Game/forca_v1.py
+++ src/04_orientacao_objetos/Lab03_Game/forca_v1.py
@@ -7,7 +7,7 @@
 import random
 
 # Board
-board = [ ''' 
+board = [''' 
 
 >>>>>>>>>>>>>>>>>> HANGMAN >>>>>>>>>>>>>>>>>>
 
@@ -65,56 +65,60 @@ board = [ '''
 /|\  |
 / \  |
      |
-=========''' ]
+=========''']
+
 
 # Class
 class Hangman:
 
-    # Contructor
+    # Constructor
     def __init__(self, word):
         self.word = word
 
     # Method to guess the letter
-    def guess(self, letter):
+    # def guess(self, letter):
 
     # Method to check if game is over
-    def hangman_over(self):
+    # def hangman_over(self):
 
     # Method to check if the player won
-    def hangman_won(self):
+    # def hangman_won(self):
 
     # Method to hide the letter on the board
-    def hide_word(self):
+    # def hide_word(self):
 
     # Method to check the status game and print the board on the screen
-    def print_game_status(self):
+    # def print_game_status(self):
+      #  print(board[0])
+
 
 # Function to read a word randomly from the word bank
 def rand_word():
-    with open('palavras.txt','rt') as f:
+    with open('palavras.txt', 'rt') as f:
         bank = f.readlines()
     return bank[random.randint(0, len(bank))].strip()
+
 
 # Main function
 def main():
     # Object
-    game = Hangman(rand_word())
-
+    game = Hangman( rand_word() )
+    print(rand_word())
     # While the game is not over, print the status, request a letter and read caracter
 
     # Check the status game
-    game.print_game_status()
+    #game.print_game_status()
 
     # According to the game status, print the message on screen to user
-    if game.hangman_won():
-        print('\nCongratulations! You won!!!')
-    else:
-        print('\nGame over! You lost.')
-        print('The word was {}'.format(game.word))
+    # if game.hangman_won():
+    #  print('\nCongratulations! You won!!!')
+    # else:
+    # print('\nGame over! You lost.')
+    # print('The word was {}'.format(game.word))
 
-    print('\nIt was good to play with you. But now go study!\n')
+    # print('\nIt was good to play with you. But now go study!\n')
+
 
 # Run program
 if __name__ == '__main__':
     main()
-
