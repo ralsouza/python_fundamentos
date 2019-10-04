408869a5ac75fd1916ee4c3224ac52c4dc6dd0ea
Rafael Lima <ls.rafael@live.com>
2019-10-04 10:31:31 -0300

    Update testes.py

--- src/04_orientacao_objetos/Lab03_Game/testes.py
+++ src/04_orientacao_objetos/Lab03_Game/testes.py
@@ -3,17 +3,18 @@ hidedWord = []
 word = 'Teste'
 letter = 'e'
 
-def hide_word(word):
-    hidedWord.append('*' * len(word))
+def hide_word():
+    hidedWord = ['*' for x in word]
     return hidedWord
 
-hide_word(word)
+hidedWord = hide_word()
 
 print('Word: ' + ''.join(hidedWord))
 
-hidedWord[2]
+hidedWord[2] = 'b'
+hidedWord[3] = 'c'
 
-
+print('Word: ' + ''.join(hidedWord))
 
 
 # if letter in word:
